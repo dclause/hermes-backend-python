89570003a172915d03471ff98e06a4c897a8349a
Dominique CLAUSE <dom.clause@gmail.com>
2022-06-08 09:15:40 +0200

    WIP: First draft at `frontend` code.

---
+++ frontend/vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from "url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+import vuetify from "vite-plugin-vuetify";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 4000,
+    host: true,
+    hmr: {
+      port: 4010,
+    },
+  },
+  plugins: [vue(), vuetify({ autoImport: true })],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
