27c6598276e83909b8c78d36161c4c37c3025f72
Dominique CLAUSE <dom.clause@gmail.com>
2022-08-29 12:40:10 +0200

    [FRONTEND] Better sidebar.

--- frontend/src/components/menus/MainMenuList.vue
+++ frontend/src/components/menus/MainMenuList.vue
@@ -1,13 +1,20 @@
 <template>
-  <v-list nav>
+  <v-list
+    class="text-center"
+    nav
+  >
     <v-tooltip>
       <template #activator="{ props }">
         <v-list-item
-          :to="{ name: 'boards'}"
-          prepend-icon="mdi-memory"
-          title="Boards"
+          :active="isRouteActive('boards') || isRouteActive('board')"
+          :to="{ name: 'board', params: { boardId: 'all' }}"
           v-bind="props"
-        />
+        >
+          <v-icon
+            icon="mdi-memory"
+            width="40"
+          />
+        </v-list-item>
       </template>
       <span>Boards</span>
     </v-tooltip>
@@ -15,13 +22,29 @@
     <v-tooltip>
       <template #activator="{ props }">
         <v-list-item
+          :active="isRouteActive('groups')"
           :to="{ name: 'devices'}"
-          prepend-icon="mdi-robot-industrial"
-          title="Groups"
           v-bind="props"
-        />
+        >
+          <v-icon
+            icon="mdi-robot-industrial"
+            width="40"
+          />
+        </v-list-item>
       </template>
       <span>Groups</span>
     </v-tooltip>
   </v-list>
 </template>
+
+<script setup>
+
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+const isRouteActive = (name) => {
+  console.log("#####", route.name === name);
+  return route.name === name;
+};
+
+</script>
